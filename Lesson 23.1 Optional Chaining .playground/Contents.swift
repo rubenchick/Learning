c226928b121ef7af6e50c4e80707081df6de997d
Anton Rubenchick <anton.rubenchik@gmail.com>
2018-04-17 23:49:06 +0300

    Lesson 23. Developers All

--- Lesson 23.1 Optional Chaining .playground/Contents.swift
+++ Lesson 23.1 Optional Chaining .playground/Contents.swift
@@ -16,7 +16,7 @@ class People {
     init(name: String) {
         self.name = name
     }
-    
+
 }
 
 class Relatives {
@@ -76,9 +76,154 @@ for item in people1.relatives! {
     case .none:       break
     }
 }
-print("У \(people1.name) братьев \(brother), сестер \(sister), дядей - \(uncle), кузин - \(cusine) ")
-//6)Создать касс животных и проперти - корова, коза, собака и тд
-//7)Создать класс растения и проперти - трава, цветы и тд
-//8)Положить их всех в массив и отсортировать по алфовиту и по классу - Люди - животные - растения
+//print("У \(people1.name) братьев \(brother), сестер \(sister), дядей - \(uncle), кузин - \(cusine) ")
 
-//print("No")
+//6)Создать касс животных и проперти - корова, коза, собака и тд
+class Animal {
+    var cow  : Cow?  = nil
+    var goat : Goat? = nil
+    var dog  : Dog?  = nil
+}
+class Cow {
+    var name: String
+    var color: String
+    init(name: String,color: String){
+        self.name = name
+        self.color = color
+    }
+    
+}
+class Goat {
+    var name: String = ""
+    var color: String = ""
+    init(name: String,color: String){
+        self.name = name
+        self.color = color
+    }
+}
+class Dog{
+    var name: String = ""
+    var type: String = ""
+    init(name: String,type: String){
+        self.name = name
+        self.type = type
+    }
+}
+
+//7)Создать класс растения и проперти - трава, цветы и тд
+class Plants {
+    var herb    : Herb? = nil
+    var flowers : Flowers? = nil
+}
+
+class Flowers{
+    var name: String = ""
+    var color: String = ""
+    init(name: String,color: String){
+        self.name = name
+        self.color = color
+    }
+}
+class Herb{
+    var name: String = ""
+    var type: String = ""
+    init(name: String,type: String){
+        self.name = name
+        self.type = type
+    }
+}
+
+//8)Положить их всех в массив и отсортировать по алфовиту и по классу - Люди - животные - растения
+var animal1 = Animal()
+var animal2 = Animal()
+var animal3 = Animal()
+var animal4 = Animal()
+var animal5 = Animal()
+var animal6 = Animal()
+var plants1 = Plants()
+var plants2 = Plants()
+var plants3 = Plants()
+var plants4 = Plants()
+var plants5 = Plants()
+var plants6 = Plants()
+animal1.cow = Cow(name: "Murka", color: "Black")
+animal2.dog = Dog(name: "Graf", type: "Kolli")
+animal3.cow = Cow(name: "Boris",color: "Brown")
+animal4.goat = Goat(name: "Zorka",color: "White")
+animal5.dog = Dog(name: "Jack", type: "Setter")
+animal6.dog = Dog(name: "Nick", type: "Lobradog")
+plants1.flowers = Flowers(name: "Lutik", color: "Blue")
+plants2.herb    = Herb(name: "Camada Green", type: "Decoration")
+plants3.flowers = Flowers(name: "Mimoza", color: "Red")
+plants4.flowers = Flowers(name: "Tilpan", color: "Yellow")
+plants5.herb    = Herb(name: "Pheniza", type: "Eating")
+plants6.flowers = Flowers(name: "Rose", color: "Dack Red")
+
+var array : [AnyObject]
+array = [people1,people2,people3,people4,animal1,plants1,animal2,plants2,animal3,plants3,
+         animal4,plants4,animal5,plants5,animal6,plants6,people5,people6,people7,people8,
+         people9,people10,people11,people12,people13,people14,people15]
+
+func printArray(_ array : [AnyObject])->() {
+    for item in array {
+        if let temp = item as? People {
+            print("People - \(temp.name)")
+        }
+        if let temp2 = item as? Animal {
+            print(temp2.cow != nil ? "Animal Cow - \(temp2.cow!.name)" : temp2.goat != nil ? "Animal Goat - \(temp2.goat!.name)" : "Animal Dog - \(temp2.dog!.name)" )
+        }
+        if let temp3 = item as? Plants {
+            print(temp3.flowers != nil ? "Plants flowers - \(temp3.flowers!.name)" : "Plants herb- \(temp3.herb!.name)")
+        }
+    }
+}
+print("Unsorted \n")
+printArray(array)
+
+array.sort {
+    switch ($0, $1) {
+    case (is People, is People) : do {
+        if let temp1 = $0 as? People {
+            if let temp2 = $1 as? People {
+                return temp1.name > temp2.name ? false : true
+            }
+            else { return false }
+        }
+        else { return  false }
+        }
+    case (is People, _)         : return true
+    case (is Animal, is People) : return false
+    case (is Animal, is Plants) : return true
+    case  (is Animal, is Animal) : do {
+        var name1 = ""
+        var name2 = ""
+        if let temp1 = $0 as? Animal {
+            name1 = temp1.cow?.name ?? (temp1.dog?.name ?? (temp1.goat?.name)!)
+        }
+        if let temp2 = $1 as? Animal {
+            name2 = temp2.cow?.name ?? (temp2.dog?.name ?? (temp2.goat?.name)!)
+        }
+        return name1 > name2 ? false : true
+        }
+    case  (is Plants, is Plants) : do {
+        var name1 = ""
+        var name2 = ""
+        if let temp1 = $0 as? Plants {
+            name1 = temp1.flowers?.name ?? (temp1.herb?.name)!
+        }
+        if let temp2 = $1 as? Plants {
+            name2 = temp2.flowers?.name ?? (temp2.herb?.name)!
+        }
+        return name1 > name2 ? false : true
+        }
+    case (is Plants,  _) : return false
+        
+    default:
+        return false
+    }
+
+}
+
+print("\nSorted \n")
+printArray(array)
+
